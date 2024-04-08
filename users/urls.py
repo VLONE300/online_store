fa69d9c4cf9d17b4cbc09f8b9c5dc73b2c5682f0
senlessed <arsentimath@gmail.com>
2024-04-08 22:03:25 +0300

    test for auth

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('activate/<str:uid>/<str:token>/', ActivateUser.as_view({'get': 'activation'}), name='activate'),
-    path('test-login/', TestLoginView.as_view())
+    path('test-login/', TestLoginView.as_view(), name='test-login')
 ]
